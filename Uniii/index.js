3629dde40ef417780fa2fa4b2d2d97e707f648cd
poby123 <youngsm12@naver.com>
2020-04-22 18:46:20 +0900

    adding animation 20200422

--- Uniii/index.js
+++ Uniii/index.js
@@ -1,26 +1,66 @@
-$(document).ready(function(){
-  var flagShow = false;
-  $('.sectionB').animate({width:'toggle'}); //처음에는 보여지지 않는 상태
+//when document is ready
+$(document).ready(function() {
 
-  var unicornAnimate = function(now,fx) {
-    $(this).css('-webkit-transform','rotate('+now+'deg)');
-    $(this).css('-moz-transform','rotate('+now+'deg)');
-    $(this).css('transform','rotate('+now+'deg)');
-  };
+  //variable
+  var flagNavShow = false; //sectionB : nav bar show flag
+  var flagLoginShow = false; //sectionLogin : Login show flag
 
+  //처음에는 보여지지 않는 상태
+  $('.sectionB').animate({width: 'toggle'});
+
+  /*title image click event*/
   $('#titleImg').click(function(){
-    $('.sectionB').animate({width:'toggle'});
-    flagShow = !flagShow;
+    if(!flagLoginShow) controlUnicornRotate();
+  });//title image click event end
 
-    if(!flagShow){
-      $('#titleImg').animate({ borderSpacing: 0 }, {
-    step: unicornAnimate, duration:'slow'},'linear');
+  /*showLoginBtn click event*/
+  $('#showLoginBtn').click(function(){
+    flagLoginShow = !flagLoginShow;
 
-    }else{
-
-      $('#titleImg').animate({ borderSpacing: -30 }, {
-    step: unicornAnimate, duration:'slow'},'linear');
-
+    if(flagNavShow){ // if nav bar is open, close the nav bar.
+      controlUnicornRotate();
     }
-  });
+    if(flagLoginShow){
+      //$('.sectionA .container').css('transform',`scale(0.5)`);
+      titleMove( [{y:0}, {y:-200}] );
+      $('.sectionLogin').css('display', 'flex');
+    }
+    else {
+      titleMove( [{y:-200}, {y:0}] );
+      $('.sectionLogin').css('display', 'none');
+    }
+  });//end showLoginBtn click event
+
+  function controlUnicornRotate() {
+   flagNavShow = !flagNavShow;
+   $('.sectionB').animate({width: 'toggle'});
+   if (!flagNavShow) {
+     unicornRotate([{borderSpacing:-30}, {borderSpacing:0}]);
+     $('.sectionA h1').css('color', 'var(--title-color)');
+   }
+   else {
+     unicornRotate([{borderSpacing:0}, {borderSpacing:-30}]);
+     $('.sectionA h1').css('color', 'white');
+   }
+  }
 });
+
+function unicornRotate(v){
+ $(v[0]).animate(v[1],{
+   duration: 300,
+   step: function(val, fx){
+     $('#titleImg').css('-webkit-transform', 'rotate(' + val + 'deg)');
+     $('#titleImg').css('-moz-transform', 'rotate(' + val + 'deg)');
+     $('#titleImg').css('transform', 'rotate(' + val + 'deg)');
+   }
+ }, 'linear');
+}
+
+function titleMove( v ){
+ $(v[0]).animate(v[1], {
+   duration: 500,
+   step: function(val) {
+       $('.sectionA .container').css("transform", `translateY(${val}px)`);
+   }
+ })
+}
